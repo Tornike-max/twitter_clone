2b7904dd2050bc89d3ba28193b965ec1d4c32d7c
Tornike-max <84152983+Tornike-max@users.noreply.github.com>
2024-07-08 12:32:07 +0400

    i added stats

--- twitter-clone/resources/views/admin/dashboard.blade.php
+++ twitter-clone/resources/views/admin/dashboard.blade.php
@@ -7,6 +7,30 @@
     </div>
     <div class="col-9">
         <h1>Admin Panel</h1>
+        <div class="row">
+            <div class="col-sm-6 col-md-4">
+                @include('shared.widget',[
+                'title'=>'Total Users',
+                'icon'=>'fas fa-users',
+                'data'=>count($totalUsers)
+                ])
+            </div>
+            <div class="col-sm-6 col-md-4">
+                @include('shared.widget',[
+                'title'=>'Total Ideas',
+                'icon'=>'fas fa-lightbulb',
+                'data'=>count($totalIdeas)
+                ])
+            </div>
+            <div class="col-sm-6 col-md-4">
+                @include('shared.widget',[
+                'title'=>'Total Comments',
+                'icon'=>'fas fa-comment',
+                'data'=>count($totalComments)
+                ])
+            </div>
+
+        </div>
     </div>
 
 </div>
---
+++ twitter-clone/resources/views/shared/widget.blade.php
@@ -0,0 +1,9 @@
+<div class="card">
+    <div class="card-body">
+        <p class="fs-5">
+            <span class="{{ $icon ?? 'fas fa-comment' }}"></span>
+            <span class="px-2"> {{ $title ?? '' }}</span>
+        </p>
+        <p class="fw-bold fs-2">{{ $data ?? 0 }}</p>
+    </div>
+</div>
